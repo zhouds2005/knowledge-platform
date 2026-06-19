7baa9df2cafd510c39d6961f9f758e49ef2f357b
系统管理员 <admin@company.com>
2026-06-19 21:41:40 +0800

    feat: add cn() utility for Tailwind class merging

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
-export function cn(...inputs: (string | undefined | null | false)[]): string {
-  return inputs.filter(Boolean).join(" ");
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
